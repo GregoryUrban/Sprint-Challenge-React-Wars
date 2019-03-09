086bb5feba34627bd7a049ae1ecde45d5ffef17a
GregoryUrban <turknben@gmail.com>
2019-03-09 14:53:47 -0500

    Stretch completed - Previous and Next buttons added with functional handlers

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
+import StarWars from './components/Character';
 import CharacterList from './components/CharacterList';
 import './App.css';
-
 
 class App extends Component {
   constructor() {
@@ -24,18 +24,41 @@ class App extends Component {
         return res.json();
       })
       .then(data => {
-        this.setState({ starwarsChars: data.results });
+        // this.setState({ starwarsChars: data.results });
+        this.setState({ starwarsChars: data.results, nextUrl : data.next, prevUrl:data.previous });
+
       })
       .catch(err => {
         throw new Error(err);
       });
   };
 
+  nextHandler=(event)=>{
+    console.log("Next handler..");
+    if (this.state.nextUrl) {
+        this.getCharacters(this.state.nextUrl);
+    }
+}
+  prevHandler=(event)=>{
+      console.log("Prev handler..");
+      if (this.state.prevUrl) {
+          this.getCharacters(this.state.prevUrl);
+      }
+  }
+
   render() {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
-        <CharacterList characters={this.state.starwarsChars} />
+          <div className="buttons">
+            <button  onClick={this.prevHandler}>Previous</button>
+            <button onClick={this.nextHandler}>Next</button>
+          </div>
+          <CharacterList characters={this.state.starwarsChars} />
+          <div className="buttons">
+            <button  onClick={this.prevHandler}>Previous</button>
+            <button onClick={this.nextHandler}>Next</button>
+          </div>
       </div>
     );
   }
