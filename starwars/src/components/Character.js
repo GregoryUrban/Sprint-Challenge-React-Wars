701bc1944cbfbc16e743d7383dec7fc3ed0faa1f
GregoryUrban <turknben@gmail.com>
2019-03-09 14:28:54 -0500

    MVP met - styling created. Although I couldnt figure out how to add Star Wars font - the attempt is commented out in css file

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Character = (props) => {
     return (
+        // why cant I map over these - seems very unDRY - Ideally I should be able to reference each
+        // item in an array bu their order number - but if I could do that 
+        //then what would be the purpose of this class component?
     <div className='character'>
-    <div className='characterInfo'>Name: {props.character.name}</div>
+    <div className='characterInfo'><h4>Name: {props.character.name}</h4></div>
     <div className='characterInfo'>Hair Color: {props.character.hair_color}</div>
     <div className='characterInfo'>Gender: {props.character.gender}</div>
     <div className='characterInfo'>Birth Year: {props.character.birth_year}</div>
