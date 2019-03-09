ff740ce00d416c284156d2b66403fbd53fdec052
GregoryUrban <turknben@gmail.com>
2019-03-09 13:02:06 -0500

    Trying to get Character and CharacterList to work - not working yet

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
-
+const Character = () => {
+    return (
+    <div className='character'>
+    <div className='characterInfo'>Name: {this.state.character.name}</div>
+    <div className='characterInfo'>Hair Color: {this.state.character.hair_color}</div>
+    <div className='characterInfo'>Gender: {this.state.character.gender}</div>
+    <div className='characterInfo'>Birth Year: {this.state.character.birth_year}</div>
+    </div>
+    );
+};
 
 
 export default Character;
--- starwars/src/components/CharacterList.js
+++ starwars/src/components/CharacterList.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import Character from './Character';
 
+const CharacterList = () => {
+    return (
+        <div className='forBlur'>
+            {this.state.characters.map(character => (
+                <Character character={character} key={character.edited} />
+            ))}
+        </div>
+    )
+}
 
 export default CharacterList;
